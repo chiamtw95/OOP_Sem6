3dd98f241e1da7c91ff2ed8d11c22042394effce
chiamtw95 <chiamtw95@gmail.com>
2021-10-05 17:46:15 +0800

    intro to scalafx

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,15 +2,35 @@ import scalafx.application.JFXApp
 import scalafx.application.JFXApp.PrimaryStage
 import scalafx.scene.Scene
 import scalafx.scene.text.Text
+import scalafx.scene.text.Font
+import scalafx.scene.effect.DropShadow
+import scalafx.scene.paint.Color
+import scalafx.scene.shape.Rectangle
 
 object Main extends JFXApp{
-    stage = new PrimaryStage() {
+    stage = new PrimaryStage() {    //window
         title = "my first scala"
         alwaysOnTop = true
         height = 400
         width = 400
-        scene = new Scene(){
-            content = new Text(10,10,"Hello World")
+        scene = new Scene(){        //
+            content = List(
+                new Text(10,50,"Hello World"){
+                font = new Font("BroadWay", 40.0)
+                effect = new DropShadow(radius = 3.0, offsetX=3.0, offsetY=3.0, color=Color.Green)
+            },
+                new Text(10,150,"Hello World"){
+                    font = new Font("BroadWay", 40.0)
+                    effect = new DropShadow(radius = 3.0, offsetX=3.0, offsetY=3.0, color=Color.Green)
+                },
+                new Rectangle() {
+                    x = 100
+                    y = 500
+                    fill = Color.Red
+                    width = 400
+                    height = 400
+                }
+            )
         }
     }
 }
